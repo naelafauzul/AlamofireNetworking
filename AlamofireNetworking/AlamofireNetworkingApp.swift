20efa672b40725c37871231adfae06fb63eaf31d
naelafauzul <naelafauzulm@gmail.com>
2024-01-22 20:05:56 +0700

    Initial Commit

---
+++ AlamofireNetworking/AlamofireNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireNetworkingApp.swift
+//  AlamofireNetworking
+//
+//  Created by Naela Fauzul Muna on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
